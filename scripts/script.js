0899bdcdfb5d2e1cd67480307222ff526b85f918
eli <e.narvydas@outlook.com>
2020-11-09 18:38:43 +0000

    created operator logic and basic page layout

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(op, a, b) {
+    switch(op) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    }
+    return result;
+}
+
+
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement('div');
+        cell.textContent = c + 1;
+        container.appendChild(cell).className = "grid-item";
+    }
+}
+//makeRows(1,4);
+
